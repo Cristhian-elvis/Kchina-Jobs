f068ba3ba569e7c14aad01bee7521b6a0538ee33
Cristhian <cristhianvv2@gmail.com>
2021-03-13 08:28:20 -0500

    nav 1.4

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,8 +6,24 @@ const showMenu = (toggleId, navId) =>{
     if(toggle && nav){
         toggle.addEventListener('click', ()=>{
             nav.classList.toggle('show-menu');
-            console.log('sdf');
         });
     }
 }
 showMenu('nav-toggle', 'nav-menu');
+
+/*===== SHOW NAV SECTION THREE =====*/
+const showNav = (toggleId, navSearchId, closeTogleId) => {
+    const toggle = document.getElementById(toggleId),
+    navSearch = document.getElementById(navSearchId),
+    closeToggle = document.getElementById(closeTogleId);
+
+    if(toggle && navSearch && closeToggle){
+        toggle.addEventListener('click', ()=>{
+            navSearch.classList.add('show-nav')
+        });
+        closeToggle.addEventListener('click', ()=>{
+            navSearch.classList.remove('show-nav')
+        })
+    }
+}
+showNav('toggle-search', 'section-three', 'close-toggle-search');
